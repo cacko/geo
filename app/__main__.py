8e1193ae7802cf3109885f8e5e4d27e0edf80c37
cacko <alex@cacko.net>
2022-12-25 22:18:44 +0000

    Whatever will be, will be 8{

--- app/__main__.py
+++ app/__main__.py
@@ -2,13 +2,18 @@ from app import create_app
 import os
 from waitress import serve
 from paste.translogger import TransLogger
-
+import logging
 
 this_files_dir = os.path.dirname(os.path.abspath(__file__))
 os.chdir(this_files_dir)
 
+host = os.environ.get("GEO_BIND_HOST", "0.0.0.0")
+port = int(os.environ.get("GEO_BIND_PORT", 21234)
+
+logging.info(f"Starting geo app {host}:{port}")
+
 serve(
     TransLogger(create_app().wsgi_app),
-    host=os.environ.get("GEO_BIND_HOST", "0.0.0.0"),
-    port=int(os.environ.get("GEO_BIND_PORT", 21234)),
+    host=host,
+    port=port),
 )
