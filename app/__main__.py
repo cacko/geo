5ccc95461b727976f593f0e6e53e7df032f59e76
cacko <alex@cacko.net>
2022-12-25 22:13:17 +0000

    this is Spartaaaaaaaa

--- app/__main__.py
+++ app/__main__.py
@@ -1,13 +1,14 @@
 from app import create_app
 import os
 from waitress import serve
+from paste.translogger import TransLogger
 
 
 this_files_dir = os.path.dirname(os.path.abspath(__file__))
 os.chdir(this_files_dir)
 
 serve(
-    create_app().wsgi_app,
+    TransLogger(create_app().wsgi_app),
     host=os.environ.get("GEO_BIND_HOST", "0.0.0.0"),
     port=int(os.environ.get("GEO_BIND_PORT", 21234)),
 )
