fcbfa7e10c874050bd4de86251dc5bedf7bdcae5
cacko <alex@cacko.net>
2023-01-26 04:15:23 +0000

    Who knows...

--- app/geo/reader/v1.py
+++ app/geo/reader/v1.py
@@ -41,6 +41,7 @@ class ASNIP(Cachable):
     def id(self):
         return snakecase(self.__ip)
 
+
 class GeoDb(object, metaclass=GeoDbMeta):
     def __init__(self) -> None:
         pth = __class__.db_root / app_config.maxmind.city_db
@@ -58,30 +59,28 @@ class GeoDb(object, metaclass=GeoDbMeta):
             return cache._struct
         res = self.__city_db.record_by_addr(ip)
         res = dict(filter(lambda x: x[1], res.items()))
-        return cache.tocache(CityInfo(
-            ip=ip,
-            country=res.get("country_name", ""),
-            country_iso=res.get("country_code", ""),
-            city=res.get("city", ""),
-            subdivisions=res.get("region_name", ""),
-            location=(res["longitude"], res["latitude"]),
-            timezone=res.get("time_zone", "")
-        ))
+        return cache.tocache(
+            CityInfo(
+                ip=ip,
+                country=res.get("country_name", ""),
+                country_iso=res.get("country_code", ""),
+                city=res.get("city", ""),
+                subdivisions=res.get("region_name", ""),
+                location=(res["longitude"], res["latitude"]),
+                timezone=res.get("time_zone", ""),
+            )
+        )
 
     def do_asn(self, ip: str):
         try:
-            assert not ll([not ip_address.ipv4(ip), not ip_address.ipv4_cidr(ip)])
+            assert not all([not ip_address.ipv4(ip), not ip_address.ipv4_cidr(ip)])
             cache = ASNIP(ip)
             if cache.load():
                 return cache._struct
             res = self.__asn_db.org_by_addr(ip)
             assert res
             asid, name = res.split(" ", 1)
-            return ASNInfo(
-                name= name,
-                id=int(asid.lstrip("AS"))
-            )
+            return ASNInfo(name=name, id=int(asid.lstrip("AS")))
         except AssertionError as e:
             logging.exception(e)
             raise ValueError
-
