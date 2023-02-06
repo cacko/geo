99753513b9ff25174cfbf1535d8bbc33573d6adf
cacko <alex@cacko.net>
2023-02-06 02:59:44 +0000

    should work now.

--- app/__main__.py
+++ app/__main__.py
@@ -3,9 +3,10 @@ from .config import app_config
 import logging
 
 uvicorn.run(
-    "app.main:app", 
-    host=app_config.server.host, 
+    "app.main:app",
+    host=app_config.server.host,
     port=app_config.server.port,
     log_level=getattr(logging, app_config.log.level),
-    reload=app_config.server.reload
+    reload=app_config.server.reload,
+    workers=app_config.server.workers
 )
--- app/config.py
+++ app/config.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel, BaseSettings, Field
 
+
 class MaxmindConfig(BaseModel):
     db: str
     city_db: str
@@ -8,25 +9,31 @@ class MaxmindConfig(BaseModel):
     city2_db: str
     asn2_db: str
 
+
 class RedisConfig(BaseModel):
     cli: str
     url: str
+
 
 class ServerConfig(BaseModel):
     host: str
     port: int
     reload: bool = Field(default=False)
+    workers: int = Field(default=1)
 
 
 class LogConfig(BaseModel):
     level: str = Field(default="INFO")
 
+
 class WebConfig(BaseModel):
     backgrounds: str
     backgrounds_path: str
 
+
 class StorageConfig(BaseModel):
     dir: str
+
 
 class FlickrConfig(BaseModel):
     key: str
