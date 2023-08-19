66f4c4ef045b68a4112b1f0be3b466f53e25cb61
cacko <alex@cacko.net>
2023-08-19 04:38:55 +0100

    I will not apologize for art.

--- app/core/ip.py
+++ app/core/ip.py
@@ -5,10 +5,10 @@ import httpx
 def get_remote_ip(req_ip, forward_ip=None):
     try:
         ipv4 = ipaddress.IPv4Address(req_ip)
-        if forward_ip:
-            return forward_ip
         if ipv4.is_private:
             return httpx.get("https://checkip.amazonaws.com").text.strip()
+        if forward_ip:
+            return forward_ip
     except Exception:
         pass
     return req_ip
