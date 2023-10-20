afe92fde50d2b5758d5a3b1305b7d2a5ced33741
cacko <alex@cacko.net>
2023-10-20 04:19:51 +0100

    Not one conflict, today was a good day.

--- app/routers/api.py
+++ app/routers/api.py
@@ -20,7 +20,10 @@ async def read_lookup(
         if not ip:
             ip = get_remote_ip(request.client.host, x_forwarded_for)
         assert validators.ip_address.ipv4(ip)
-        return MaxMind.lookup(ip).model_dump()
+        res = MaxMind.lookup(ip)
+        if res.location:
+            return GeoCoder.from_gps(*res.location).model_dump()
+        return res.model_dump()
     except AssertionError:
         raise HTTPException(status_code=502)
 
