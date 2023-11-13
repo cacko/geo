457f5349359810c86b157735754cab74b38efac5
cacko <alex@cacko.net>
2023-11-13 15:55:12 +0000

    Nitpicking about alphabetizing methods, minor OCD thing

--- app/__main__.py
+++ app/__main__.py
@@ -2,18 +2,19 @@ import uvicorn
 from .config import app_config
 from app.main import create_app
 import sys
+import asyncio
+from hypercorn.config import Config
+from hypercorn.asyncio import serve
 
 if len(sys.argv) == 1:
 
-    server_config = uvicorn.Config(
-        app=create_app,
-        host=app_config.server.host,
-        port=app_config.server.port,
+    server_config = Config.from_mapping(
+        bind=f"{app_config.server.host}:{app_config.server.port}",
         workers=app_config.server.workers,
-        factory=True
+        worker_class="trio"
     )
     server = uvicorn.Server(server_config)
-    server.run()
+    asyncio.run(serve(create_app, server_config))
 else:
     import app.cli
     app.cli.run()
