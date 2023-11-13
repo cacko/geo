73c5fe443220a9d7fca46c594076c0eb06b48216
cacko <alex@cacko.net>
2023-11-13 13:50:15 +0000

    Added another dependency

--- app/__main__.py
+++ app/__main__.py
@@ -8,7 +8,6 @@ if len(sys.argv) == 1:
         "app.main:app",
         host=app_config.server.host,
         port=app_config.server.port,
-        log_level=getattr(logging, app_config.log.level),
         reload=app_config.server.reload,
         workers=app_config.server.workers
     )
