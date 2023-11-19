2aec26a149e6ea4c66fd0e9be62c543bc27457d1
cacko <alex@cacko.net>
2023-11-19 10:57:24 +0000

    Reticulating splines...

--- app/__main__.py
+++ app/__main__.py
@@ -1,4 +1,3 @@
-import uvicorn
 from .config import app_config
 from app.main import app
 import sys
@@ -10,10 +9,8 @@ if len(sys.argv) == 1:
 
     server_config = Config.from_mapping(
         bind=f"{app_config.server.host}:{app_config.server.port}",
-        # workers=app_config.server.workers,
         worker_class="trio"
     )
-    server = uvicorn.Server(server_config)
     asyncio.run(serve(app, server_config))
 else:
     import app.cli
