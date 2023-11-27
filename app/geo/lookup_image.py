a41b1ba5fc80ccb15c59eb93998f4c558b81a384
cacko <alex@cacko.net>
2023-11-27 03:35:21 +0000

    Fixed so the code compiles

--- app/geo/lookup_image.py
+++ app/geo/lookup_image.py
@@ -18,7 +18,7 @@ class LookupImageParams(BaseModel):
     guidance_scale: float = Field(default=4)
     num_inference_steps: int = Field(default=4)
     seed: Optional[int] = None
-    model: str = Field(default="lcm_xl")
+    model:  Optional[str] = None
     upscale: bool = Field(default=True)
 
 
